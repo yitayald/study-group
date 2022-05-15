c8cb20fc257963177a8348dedf1467bda725ee0e
yitayald <yitayaldebalke@gmail.com>
2022-05-14 21:02:59 -0400

    commit fromeclips2

---
+++ study group2/src/example2/paractice2.java
@@ -0,0 +1,11 @@
+package example2;
+
+public class paractice2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("welcome to version control class");
+		System.out.println("welcome to webservice technology");
+	}
+
+}
