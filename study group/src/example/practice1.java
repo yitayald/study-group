c55e1524f496d07d791427955c7c6df6dc9ce2dc
yitayald <yitayaldebalke@gmail.com>
2022-05-14 20:55:29 -0400

    commit from eclips

---
+++ study group/src/example/practice1.java
@@ -0,0 +1,13 @@
+package example;
+
+public class practice1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("welcome to bach 26");
+System.out.println("welcome to april 11  bach 26");
+System.out.println("welcome to version control");
+System.out.println("welcome to webservice technology");
+	}
+
+}
